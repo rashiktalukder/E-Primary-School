fa7236380a4e9a8adeeee744fe4efdc32a8375e0
rashiktalukder <Kawsar.Rashik@gmail.com>
2021-12-16 01:20:58 +0600

    Working on Employees table

---
+++ E-Primary-School/Controllers/EmployeesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Primary_School.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
