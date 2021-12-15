f57470770af8ba30555e8f57fd392060d0132cdf
rashiktalukder <Kawsar.Rashik@gmail.com>
2021-12-15 23:32:47 +0600

    User Model Entity ready...

---
+++ E-Primary-School/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Primary_School.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string userId { get; set; }
+        [Required]
+        public string password { get; set; }
+        [Required]
+        public int userType { get; set; }
+
+
+
+
+    }
+}
