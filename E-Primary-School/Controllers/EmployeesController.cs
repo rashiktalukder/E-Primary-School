7cd881cb640dd82cde1a7daeb9274ce3086d3a29
Shihab Ahmed Efty <shihabahmed620@gmail.com>
2021-12-16 02:25:08 +0600

    add empoyee controller

--- E-Primary-School/Controllers/EmployeesController.cs
+++ E-Primary-School/Controllers/EmployeesController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_Primary_School.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace E_Primary_School.Controllers
 {
     public class EmployeesController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Employee Employee { get; set; }
+
+        public EmployeesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+        #region API Calls
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Employees.ToListAsync() });
+        }
     }
+    #endregion
 }
