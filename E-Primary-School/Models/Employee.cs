ea5607cbda80231b4c5ab17921a1d63f691112f5
Shihab Ahmed Efty <shihabahmed620@gmail.com>
2021-12-15 23:32:56 +0600

    Create Employee Model

---
+++ E-Primary-School/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Primary_School.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string userId { get; set; }
+        [Required]
+        public string empName { get; set; }
+        [Required(ErrorMessage = "Email address can not empty")]
+        public string empEmail { get; set; }
+        [Required]
+        public string empPhoneNumber { get; set; }
+        [Required]
+        public int empSalary { get; set; }
+        [Required]
+        public int userType { get; set; }
+    }
+}
